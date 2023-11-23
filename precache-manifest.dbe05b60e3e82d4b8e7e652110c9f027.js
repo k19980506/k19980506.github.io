f473693f1e8848bb41e9f1d94fb4f8253f4a72cc
King <k19980506@gmail.com>
2023-11-23 22:40:45 +0800

    Updates

---
+++ precache-manifest.dbe05b60e3e82d4b8e7e652110c9f027.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d41c9da5a7af72735d689e45a7780c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c197c86ba476194202",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5f8fffd221162de2201",
+    "url": "/static/js/2.b13011a2.chunk.js"
+  },
+  {
+    "revision": "653ac1c43bf30a302d9476ac1d639668",
+    "url": "/static/js/2.b13011a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b95030a0dae8690577",
+    "url": "/static/js/3.d4060ec8.chunk.js"
+  },
+  {
+    "revision": "50c197c86ba476194202",
+    "url": "/static/js/main.4491eee9.chunk.js"
+  },
+  {
+    "revision": "3fa0c3c6d57352dc6093",
+    "url": "/static/js/runtime-main.aeff8b0b.js"
+  },
+  {
+    "revision": "93db1cf5678e55fd26b2485110167b95",
+    "url": "/static/media/index.93db1cf5.cjs"
+  }
+]);
