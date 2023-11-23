9ccdaf57d1fb3b6b5d58869d3c55fc309b1fd8ef
King <k19980506@gmail.com>
2023-11-23 22:38:47 +0800

    Add message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Message from "./Message";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter basename="/k19980506.github.io">
+			<Routes>
+				<Route path="/message" element={<Message />} />
+				<Route path="/" exact element={<App />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
 
